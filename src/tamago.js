54a33e86ed897f244fb8d44fe24a7a95efdbc4fe
Hannes Braun <hannesbraun@mail.de>
2021-03-02 17:20:56 +0100

    Fix formatting

--- src/tamago.js
+++ src/tamago.js
@@ -15,7 +15,8 @@ function tamagoClick() {
 }
 
 function pad(num, size) {
-    num = num.toString();
-    while (num.length < size) num = "0" + num;
-    return num;
+	num = num.toString();
+	while (num.length < size)
+		num = "0" + num;
+	return num;
 }
